4ee54203240db079e9e332b61ac3273cd5714a07
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2024-10-25 15:18:10 -0300

    life zero bug when restarting the game from the main menu fixed

--- RPP Project/Assets/Scripts/UI Elements/MainMenu.cs
+++ RPP Project/Assets/Scripts/UI Elements/MainMenu.cs
@@ -20,6 +20,7 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("TestScene");
+        GameManager.instance.hp.SetupHealth();
     }
 
     public void QuitGame()
