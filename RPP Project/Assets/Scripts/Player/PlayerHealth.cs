fad808105aaa6182f8ef80067741b8205ce21e7f
raphael <raphaelqueiroz775@gmail.com>
2024-11-18 23:07:56 -0300

    death animation

--- RPP Project/Assets/Scripts/Player/PlayerHealth.cs
+++ RPP Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -10,7 +10,7 @@ public class PlayerHealth : MonoBehaviour
     //public int healAmount;
     public HealthBar bar;
     public GameObject player;
-    public Animator anim;
+    //public Animator anim;
     
     
 
@@ -36,9 +36,9 @@ public class PlayerHealth : MonoBehaviour
         
         currentHealth -= damageTaken;
         bar.SetHealth(currentHealth);
-        anim.SetBool("isHit", true);
-        
-        if(currentHealth <= 0) StartCoroutine("PlayerDeath");
+        GameManager.instance.playerMove.anim.SetBool("isHit", true);
+
+        if (currentHealth <= 0) StartCoroutine("PlayerDeath");
     }
     
     public IEnumerator PlayerDeath()
@@ -46,15 +46,15 @@ public class PlayerHealth : MonoBehaviour
         
         currentHealth = 0;
         bar.SetHealth(currentHealth);
-        
+        GameManager.instance.playerMove.anim.SetBool("isDead", true);
+        GameManager.instance.playerMove.anim.SetBool("isHit", false);
+        GameManager.instance.playerMove.anim.SetBool("isWalking", false);
         GameManager.instance.playerShoot.enabled = false;
         GameManager.instance.playerMove.enabled = false;
         GameManager.instance.playerMove.rb.velocity = Vector2.zero;
         yield return new WaitForSeconds(3f);
         SceneManager.LoadScene("GameOver");
-        anim.SetBool("isDead", true);
-        anim.SetBool("isWalking", false);
-        anim.SetBool("isHit", false);
+        
     }
 
     public void PlayerHeal(int healAmount)
